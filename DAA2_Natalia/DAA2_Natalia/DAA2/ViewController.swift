ec007fb2a612ef443fad9177353d1f7ad1dbeda1
Natalia Vicente Sánchez <145784657+nataliavicente19@users.noreply.github.com>
2023-10-03 12:12:51 -0700

    Add files via upload

---
+++ DAA2_Natalia/DAA2_Natalia/DAA2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DAA2
+//
+//  Created by nati on 22/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
